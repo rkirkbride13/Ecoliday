74eafb3debd8107b2aff9db2491164771b067cce
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-30 16:16:17 +0000

    Add Tailwind to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="bg-red-500 border-8">Hello world</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
