86fa994d1be4fb1a06a28e4526919c0ca9e223a4
Graeme Paterson <ghpaterson@gmail.com>
2023-01-31 14:44:16 +0000

    mocking emissions test - passing jest

--- api/controllers/emissions.js
+++ api/controllers/emissions.js
@@ -1,6 +1,40 @@
+const apiKey = require("../apiKey");
+
 const EmissionsController = {
   GetPlaneEmissions: (req, res) => {
-    res.status(200).json({ message: "ok" });
+    let data = "";
+    const URL = "https://beta3.api.climatiq.io/estimate";
+    fetch(URL, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        emission_factor: {
+          activity_id:
+            "passenger_flight-route_type_international-aircraft_type_na-distance_short_haul_lt_3700km-class_na-rf_included",
+          source: "GHG Protocol",
+          region: "GB",
+          year: "2021",
+          lca_activity: "fuel_combustion",
+        },
+        parameters: {
+          passengers: parseInt(req.query.passengers),
+          distance: parseInt(req.query.distance),
+          distance_unit: "km",
+        },
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        data = responseData;
+        console.log(responseData);
+        res.status(200).json({ message: "ok", co2e: data.co2e });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
 };
 
--- api/spec/controllers/emissions.spec.js
+++ api/spec/controllers/emissions.spec.js
@@ -5,15 +5,38 @@ require("jest-fetch-mock").enableMocks();
 describe("/emissions", () => {
   describe("/plane", () => {
     test("response code is 200", async () => {
+      fetch.mockResponse(
+        JSON.stringify({
+          co2e: 63.094792,
+        })
+      );
       let response = await request(app).get("/emissions/plane");
       expect(response.status).toEqual(200);
     });
 
     test("response code is 200 when given distance and passengers params", async () => {
+      fetch.mockResponse(
+        JSON.stringify({
+          co2e: 63.094792,
+        })
+      );
       let response = await request(app).get(
         "/emissions/plane?distance=100&passengers=1"
       );
       expect(response.status).toEqual(200);
+    });
+
+    test("calls fetch and loads the co2e value", async () => {
+      fetch.mockResponse(
+        JSON.stringify({
+          co2e: 63.094792,
+        })
+      );
+      let response = await request(app).get(
+        "/emissions/plane?distance=100&passengers=1"
+      );
+      expect(response.status).toEqual(200);
+      expect(response.body.co2e).toEqual(63.094792);
     });
   });
 });
