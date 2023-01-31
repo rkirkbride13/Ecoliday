f69d5e0e3338f67867f791ed32b1ff8f834af2c3
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-31 16:35:39 +0000

    Remove comments from app.js

--- api/app.js
+++ api/app.js
@@ -11,32 +11,4 @@ const emissionsRouter = require("./routes/emissions");
 //route setup
 app.use("/emissions", emissionsRouter);
 
-//catch 404 and forward to error handler
-
-//error handler
-
-// app.use(require("./routes/record"));
-// get driver connection
-// const dbo = require("./db/conn");
-
-// app.listen(port, () => {
-//   // perform a database connection when server starts
-//   dbo.connectToServer(function (err) {
-//     if (err) console.error(err);
-//   });
-//   console.log(`Server is running on port: ${port}`);
-// });
-
-// const Temp = require("./models/test");
-
-// app.post("/temp", async (req, res) => {
-//   await Temp.create({ message: req.body.message });
-//   res.status(200).json({ message: "OK" });
-// });
-
-// app.get("/temp", async (req, res) => {
-//   const messages = await Temp.find({});
-//   res.status(200).json({ messages: messages });
-// });
-
 module.exports = app;
