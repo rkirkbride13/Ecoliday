3774b9e4c8b9c365bad2c8845c2cbd42cd9d8e1d
Graeme Paterson <ghpaterson@gmail.com>
2023-01-31 12:29:54 +0000

    test for response code 200 on emissions/plane with params and without

--- api/controllers/emissions.js
+++ api/controllers/emissions.js
@@ -1,5 +1,7 @@
 const EmissionsController = {
-  GetPlaneEmissions: (req, res) => {},
+  GetPlaneEmissions: (req, res) => {
+    res.status(200).json({ message: "ok" });
+  },
 };
 
 module.exports = EmissionsController;
--- api/spec/controllers/emissions.spec.js
+++ api/spec/controllers/emissions.spec.js
@@ -4,7 +4,12 @@ require("jest-fetch-mock").enableMocks();
 
 describe("/emissions", () => {
   describe("/plane", () => {
-    test("response code is 200 when params distance and passengers given", async () => {
+    test("response code is 200", async () => {
+      let response = await request(app).get("/emissions/plane");
+      expect(response.status).toEqual(200);
+    });
+
+    test("response code is 200 when given distance and passengers params", async () => {
       let response = await request(app).get(
         "/emissions/plane?distance=100&passengers=1"
       );
