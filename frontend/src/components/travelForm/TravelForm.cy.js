877f9446278fb4fe1369f45f812c50657d083295
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-31 11:35:35 +0000

    Add test for typing in fields and sending request

--- frontend/src/components/travelForm/TravelForm.cy.js
+++ frontend/src/components/travelForm/TravelForm.cy.js
@@ -1,21 +1,31 @@
-import TravelForm from './TravelForm'
+import TravelForm from "./TravelForm";
 
-describe('TravelForm', () => {
+describe("TravelForm", () => {
+  beforeEach(() => {
+    cy.mount(<TravelForm />);
+  });
 
-  it('has distance and no of people inputs and a submit buttom', () =>{
-
-    cy.mount(<TravelForm/>)
-
+  it("has distance and no of people inputs and a submit buttom", () => {
     cy.get('[data-cy="distance"]')
-    .invoke('attr', 'placeholder')
-    .should('contain', 'Distance in km')
+      .invoke("attr", "placeholder")
+      .should("contain", "Distance in km");
 
-    cy.get('[data-cy="numPeople"]')
+    cy.get('[data-cy="passengers"]');
 
-    cy.get('[data-cy="travelFormSubmit"')
-    .invoke('attr', 'submit')
-    .should('eq', 'submit')
+    cy.get('[data-cy="travelFormSubmit"]')
+      .invoke("attr", "type")
+      .should("eq", "submit");
+  });
 
-  })
+  it("user can type in the field", () => {
+    cy.intercept("GET", "/emissions/plane?distance=1000&passengers=2").as(
+      "emissionRequest"
+    );
 
-})
+    cy.get('[data-cy="distance"]').type("1000");
+    cy.get('[data-cy="passengers"]').type("2");
+    cy.get('[data-cy="travelFormSubmit"]').click();
+
+    cy.wait("@emissionRequest");
+  });
+});
--- frontend/src/components/travelForm/TravelForm.js
+++ frontend/src/components/travelForm/TravelForm.js
@@ -1,7 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const TravelForm = ( props ) => {
+const TravelForm = (props) => {
+  const [distance, setDistance] = useState("");
+  const [passengers, setPassengers] = useState("");
 
-}
+  const handleChange = (setFunction) => {
+    return (event) => {
+      setFunction(event.target.value);
+    };
+  };
 
-export default TravelForm
+  return (
+    <div id="travelForm">
+      <form>
+        <input
+          data-cy="distance"
+          type="text"
+          placeholder="Distance in km"
+          min="0.0"
+          max="20000.0"
+          value={distance}
+          onChange={handleChange(setDistance)}
+        />
+
+        <input
+          data-cy="passengers"
+          type="text"
+          min="1"
+          value={passengers}
+          onChange={handleChange(setPassengers)}
+        />
+
+        <input data-cy="travelFormSubmit" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default TravelForm;
