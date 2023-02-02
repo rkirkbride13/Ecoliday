c754f4151c4dbce1384e3ee5ae650a78de70cb7b
Marina Papapetrou <contact@marinapapap.com>
2023-02-02 12:33:06 +0000

    Set-up DistanceController and passed test 'sends a request when given a from location'.

---
+++ api/controllers/distance.js
@@ -0,0 +1,11 @@
+const DistanceController = {
+  Calculate: async (req, res, next) => {
+    await fetch(
+      `https://geocode.xyz?locate=${req.query.from}&json=1auth=${process.env.GEOCODE_KEY}`
+    )
+      .then((response) => response.json())
+      .catch((err) => console.error(err));
+  },
+};
+
+module.exports = DistanceController;
---
+++ api/spec/controllers/distanceCalculator.test.js
@@ -0,0 +1,25 @@
+const DistanceController = require("../../controllers/distance");
+require("jest-fetch-mock").enableMocks();
+
+describe("DistanceController", () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+    fetch.mockResponse("{}");
+  });
+
+  it("sends a request when given a 'from' location", async () => {
+    const req = {
+      query: {
+        to: "Berlin",
+        from: "London",
+        passengers: "2",
+      },
+    };
+
+    await DistanceController.Calculate(req, {}, () => {});
+
+    expect(fetch).toHaveBeenCalledWith(
+      expect.stringContaining("https://geocode.xyz?locate=London&json=1")
+    );
+  });
+});
