2cdbc67772a4b4b7c7f6a0f40d7aeedc24b35933
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-02 14:02:46 +0000

    Passed test to get api response for to query pararmeter

--- api/controllers/distance.js
+++ api/controllers/distance.js
@@ -5,6 +5,11 @@ const DistanceController = {
     )
       .then((response) => response.json())
       .catch((err) => console.error(err));
+    await fetch(
+      `https://geocode.xyz?locate=${req.query.to}&json=1auth=${process.env.GEOCODE_KEY}`
+    )
+      .then((response) => response.json())
+      .catch((err) => console.error(err));
   },
 };
 
--- api/spec/controllers/distanceCalculator.test.js
+++ api/spec/controllers/distanceCalculator.test.js
@@ -22,4 +22,20 @@ describe("DistanceController", () => {
       expect.stringContaining("https://geocode.xyz?locate=London&json=1")
     );
   });
+
+  it("sends a request when given a 'to' location", async () => {
+    const req = {
+      query: {
+        to: "Berlin",
+        from: "London",
+        passengers: "2",
+      },
+    };
+
+    await DistanceController.Calculate(req, {}, () => {});
+
+    expect(fetch).toHaveBeenCalledWith(
+      expect.stringContaining("https://geocode.xyz?locate=Berlin&json=1")
+    );
+  });
 });
