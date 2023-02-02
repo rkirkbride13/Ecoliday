fff9c6fc0fc367128a62732b9b9ac867c1631b65
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-02 14:30:40 +0000

    Passed test to pass distance result to distance query parameter

--- api/controllers/distance.js
+++ api/controllers/distance.js
@@ -1,15 +1,41 @@
 const DistanceController = {
   Calculate: async (req, res, next) => {
-    locationData = await Promise.all(
+    const locationData = await Promise.all(
       [req.query.from, req.query.to].map((location) => {
-        fetch(
+        return fetch(
           `https://geocode.xyz?locate=${location}&json=1auth=${process.env.GEOCODE_KEY}`
         )
           .then((response) => response.json())
           .catch((err) => console.error(err));
       })
     );
+
+    req.query.distance = getDistance(
+      locationData[0].latt,
+      locationData[0].longt,
+      locationData[1].latt,
+      locationData[1].longt
+    );
   },
 };
+
+function getDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
 
 module.exports = DistanceController;
--- api/spec/controllers/distanceCalculator.test.js
+++ api/spec/controllers/distanceCalculator.test.js
@@ -2,20 +2,25 @@ const DistanceController = require("../../controllers/distance");
 require("jest-fetch-mock").enableMocks();
 
 describe("DistanceController", () => {
+  let req;
   beforeEach(() => {
     fetch.resetMocks();
-    fetch.mockResponse("{}");
-  });
-
-  it("sends a request when given a 'from' location", async () => {
-    const req = {
+    fetch.mockResponseOnce(
+      JSON.stringify({ longt: "-0.11534", latt: "51.51413" })
+    );
+    fetch.mockResponseOnce(
+      JSON.stringify({ longt: "13.40488", latt: "52.50176" })
+    );
+    req = {
       query: {
         to: "Berlin",
         from: "London",
         passengers: "2",
       },
     };
+  });
 
+  it("sends a request when given a 'from' location", async () => {
     await DistanceController.Calculate(req, {}, () => {});
 
     expect(fetch).toHaveBeenCalledWith(
@@ -24,18 +29,16 @@ describe("DistanceController", () => {
   });
 
   it("sends a request when given a 'to' location", async () => {
-    const req = {
-      query: {
-        to: "Berlin",
-        from: "London",
-        passengers: "2",
-      },
-    };
-
     await DistanceController.Calculate(req, {}, () => {});
 
     expect(fetch).toHaveBeenCalledWith(
       expect.stringContaining("https://geocode.xyz?locate=Berlin&json=1")
     );
+  });
+
+  it("add distance to req.query", async () => {
+    await DistanceController.Calculate(req, {}, () => {});
+
+    expect(req.query.distance).toEqual(930.5084324079236);
   });
 });
