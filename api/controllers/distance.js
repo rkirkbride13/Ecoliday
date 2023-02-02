0872f63f7a20301d660c9d6ff812a64055ee225f
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-02 14:11:33 +0000

    Refactored Calculate method and passed existing tests

--- api/controllers/distance.js
+++ api/controllers/distance.js
@@ -1,15 +1,14 @@
 const DistanceController = {
   Calculate: async (req, res, next) => {
-    await fetch(
-      `https://geocode.xyz?locate=${req.query.from}&json=1auth=${process.env.GEOCODE_KEY}`
-    )
-      .then((response) => response.json())
-      .catch((err) => console.error(err));
-    await fetch(
-      `https://geocode.xyz?locate=${req.query.to}&json=1auth=${process.env.GEOCODE_KEY}`
-    )
-      .then((response) => response.json())
-      .catch((err) => console.error(err));
+    locationData = await Promise.all(
+      [req.query.from, req.query.to].map((location) => {
+        fetch(
+          `https://geocode.xyz?locate=${location}&json=1auth=${process.env.GEOCODE_KEY}`
+        )
+          .then((response) => response.json())
+          .catch((err) => console.error(err));
+      })
+    );
   },
 };
 
