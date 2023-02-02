fcd1fadb93717f6f10d4e760ef9c871114b6ec50
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-02-02 21:41:54 +0000

    Emission results formatting fixed so context is seen to right on hover

--- frontend/src/components/emissionResults/emissionResults.js
+++ frontend/src/components/emissionResults/emissionResults.js
@@ -12,53 +12,59 @@ const EmissionResults = ({ emissions, renderEmissions }) => {
     ];
 
     let resultDivs = resultsArray.map((result) => (
-      <div className="dropdown dropdown-hover dropdown-right overflow-visible">
-        <ul
-          tabIndex={0}
-          className="dropdown-content menu p-2 shadow-2xl shadow-green-700 bg-green-500 rounded-box w-40 font-bold text-xl text-black text-center"
-        >
-          <li>
-            {`${Math.ceil(result.emissions.total / CO2eSteak)} ${
-              Math.ceil(result.emissions.total / CO2eSteak) === 1
-                ? "Steak"
-                : "Steaks"
-            }`}
-          </li>
-          <li>
-            {`${Math.ceil(result.emissions.total / CO2eTshirt)} ${
-              Math.ceil(result.emissions.total / CO2eTshirt) === 1
-                ? "T-shirt"
-                : "T-shirts"
-            }`}
-          </li>
-          <li>
-            {`${Math.ceil(result.emissions.total / CO2eTree)} ${
-              Math.ceil(result.emissions.total / CO2eTree) === 1
-                ? "Tree"
-                : "Trees"
-            }`}
-          </li>
-        </ul>
-        <div className="stat shadow">    
-        <div className="stat">
-          <div className="stat-title">CO2e by {result.type}</div>
-          <div
-            data-cy={`total-emissions-${result.type}`}
-            className="stat-value"
-          >{`${result.emissions.total.toFixed(1)} kg`}</div>
-          <div
-            data-cy={`person-emissions-${result.type}`}
-            className="stat-desc"
-          >{`Per Person: ${result.emissions.perPassenger.toFixed(1)} kg`}</div>
-        </div>
+      <div>
+        <div className="dropdown dropdown-hover dropdown-right mb-2">
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow-2xl shadow-green-700 bg-green-500 rounded-box w-60 text-base font-bold text-white text-left ml-2"
+          >
+            Equivalent to...
+            <li>
+            &#x1F42E;{`  Eating ${Math.ceil(result.emissions.total / CO2eSteak)} ${
+                Math.ceil(result.emissions.total / CO2eSteak) === 1
+                  ? "steak"
+                  : "steaks"
+              }`}
+            </li>
+            <li>
+            &#x1F455;{`  Buying ${Math.ceil(result.emissions.total / CO2eTshirt)} ${
+                Math.ceil(result.emissions.total / CO2eTshirt) === 1
+                  ? "T-shirt"
+                  : "T-shirts"
+              }`}
+            </li>
+            <li>
+            &#x1F333;{`  Saving ${Math.ceil(result.emissions.total / CO2eTree)} ${
+                Math.ceil(result.emissions.total / CO2eTree) === 1
+                  ? "tree"
+                  : "trees"
+              } this year`}
+            </li>
+          </ul>
+
+          <div className="stats border">
+            <div className="stat w-60">
+              <div className="stat-title">CO2e by {result.type}</div>
+              <div
+                data-cy={`total-emissions-${result.type}`}
+                className="stat-value"
+              >{`${result.emissions.total.toFixed(1)} kg`}</div>
+              <div
+                data-cy={`person-emissions-${result.type}`}
+                className="stat-desc"
+              >{`Per Person: ${result.emissions.perPassenger.toFixed(
+                1
+              )} kg`}</div>
+            </div>
+          </div>
         </div>
       </div>
     ));
 
     return (
       <>
-        <div id="emissionResults" className="mt-2">
-            <div>{resultDivs}</div>
+        <div id="emissionResults">
+          <div>{resultDivs}</div>
         </div>
       </>
     );
