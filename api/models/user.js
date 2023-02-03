116a1f77689ffef39ca9ff683d4907b5a8da5072
Graeme Paterson <ghpaterson@gmail.com>
2023-02-03 16:16:29 +0000

    handleSubmit and handleChange setup - successful submit navigates to homepage

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type:String, required: true, unique: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/] },
-  password: { type:String, required: true }
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+  },
+  password: { type: String, required: true },
+});
 
-const User = mongoose.model("User", UserSchema)
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
