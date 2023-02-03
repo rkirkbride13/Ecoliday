227dcfbe7704e51cbcc4373b8a8c3003ae47ecbf
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-02-03 16:40:40 +0000

    Unit tests for SignUpForm and e2e test for User signing up and being redirected passing

---
+++ frontend/cypress/e2e/user_signup.cy.js
@@ -0,0 +1,26 @@
+describe("Signing up", () => {
+  it("with valid credentials, redirects to '/'", () => {
+    cy.visit("/signup");
+    cy.get('[data-cy="email"]').type("someone@example.com");
+    cy.get('[data-cy="password"]').type("password");
+    cy.get('[data-cy="signup-submit"]').click();
+
+    cy.url().should("include", "/");
+  });
+
+  it("with missing password, stays on '/signup'", () => {
+    cy.visit("/signup");
+    cy.get('[data-cy="email"]').type("someone@example.com");
+    cy.get('[data-cy="signup-submit"]').click();
+
+    cy.url().should("include", "/signup");
+  });
+
+  it("with missing email, stays on '/signup'", () => {
+    cy.visit("/signup");
+    cy.get('[data-cy="password"]').type("password");
+    cy.get('[data-cy="signup-submit"]').click();
+
+    cy.url().should("include", "/signup");
+  });
+});
