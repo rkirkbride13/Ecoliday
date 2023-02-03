cd7757dc5b5c6e9cc5a388604571443ddeda90f6
Graeme Paterson <ghpaterson@gmail.com>
2023-02-03 16:20:02 +0000

    handleSubmit and handleChange added

--- frontend/src/components/signUp/signUpForm.js
+++ frontend/src/components/signUp/signUpForm.js
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
 
 const SignUpForm = ({ navigate }) => {
+  const handleChange = (setFunction) => {
+    return (event) => {
+      setFunction(event.target.value);
+    };
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    fetch("/users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    }).then((response) => {
+      if (response.status === 201) {
+        console.log("success");
+        navigate("/");
+      } else {
+        console.log("error");
+        navigate("/signup");
+      }
+    });
+  };
+
   return (
     <>
-      <form >
-      {/* <form onSubmit={handleSubmit}> */}
+      <form onSubmit={handleSubmit}>
         <div id="email" className="mb-5 text-xl mx-auto">
           <label for="email"></label>
           <br />
@@ -13,8 +39,8 @@ const SignUpForm = ({ navigate }) => {
             className="pl-1 border-2 rounded peer"
             data-cy="email"
             type="text"
-            // value={email}
-            // onChange={handleChange(setEmail)}
+            value={email}
+            onChange={handleChange(setEmail)}
           />
         </div>
         <div id="password" className="mb-5 text-xl mx-auto">
@@ -25,8 +51,8 @@ const SignUpForm = ({ navigate }) => {
             className="pl-1 border-2 rounded peer"
             data-cy="password"
             type="password"
-            // value={password}
-            // onChange={handleChange(setPassword)}
+            value={password}
+            onChange={handleChange(setPassword)}
           />
         </div>
         <div>
