de942e4262bf66173333d9f35a728046c83c1b7c
Marina Papapetrou <contact@marinapapap.com>
2023-02-06 12:30:28 +0000

    Created TokensController.Create function and route. Tested for invalid and valid user details provided.

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// const JWT = require("jsonwebtoken");
 require("dotenv").config({ path: "./config.env" });
 // const port = process.env.PORT || 5000;
 app.use(cors());
@@ -8,9 +9,11 @@ app.use(express.json());
 
 const emissionsRouter = require("./routes/emissions");
 const usersRouter = require("./routes/users");
+const tokensRouter = require("./routes/tokens");
 
 //route setup
 app.use("/emissions", emissionsRouter);
 app.use("/users", usersRouter);
+app.use("/tokens", tokensRouter);
 
 module.exports = app;
