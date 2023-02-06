1c4a4828ebba12caaec0e6b896f4ce9e8e9818b8
Graeme Paterson <ghpaterson@gmail.com>
2023-02-06 11:59:22 +0000

    token generator model created and test passing. JWT secret assigned in env file

---
+++ api/models/token_generator.js
@@ -0,0 +1,16 @@
+const JWT = require("jsonwebtoken");
+const options = { expiresIn: "10m" };
+require("dotenv").config({ path: ".env" });
+const secret = process.env.JWT_SECRET;
+
+class TokenGenerator {
+  static jsonwebtoken(user_id) {
+    return JWT.sign(
+      { user_id: user_id, iat: Math.floor(Date.now() / 1000) },
+      secret,
+      options
+    );
+  }
+}
+
+module.exports = TokenGenerator;
