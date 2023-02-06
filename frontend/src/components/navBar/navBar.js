559dcbeee6c39a50c9524a05dd52852be42df991
Graeme Paterson <ghpaterson@gmail.com>
2023-02-06 15:33:53 +0000

    initial login button created

--- frontend/src/components/navBar/navBar.js
+++ frontend/src/components/navBar/navBar.js
@@ -1,8 +1,16 @@
 const NavBar = (props) => {
   return (
-    <div className="navbar bg-green-500 fixed w-full top-0">
-      <h1 className=" text-white text-3xl font-bold ml-6">Ecoliday</h1>
-    </div>
+    <>
+      <div className="navbar bg-green-500 fixed w-full top-0">
+        <h1 className=" text-white text-3xl font-bold ml-6">Ecoliday</h1>
+        <input
+          data-cy="travelFormSubmit"
+          type="submit"
+          value="Submit"
+          className="btn bg-white border-0 text-green-500 rounded-full"
+        />
+      </div>
+    </>
   );
 };
 
