5e85dffa9c997b8e32889b37096401b388ad63f0
Graeme Paterson <ghpaterson@gmail.com>
2023-02-06 14:25:14 +0000

    new Login Route created on app.js and loginForm component created and unit test passes

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-import TravelForm from "../travelForm/TravelForm";
-import NavBar from "../navBar/navBar";
-import EmissionResults from "../emissionResults/emissionResults";
+
 import { Routes, Route, useNavigate } from "react-router-dom";
 import SignUpForm from "../signUp/signUpForm";
 import HomePage from "../homepage/homepage";
+import LoginForm from "../login/loginForm";
 
 const App = () => {
-
   return (
-      <Routes>
-        <Route path="/" element={<HomePage navigate={ useNavigate()}/>} />
-        <Route path="/signup" element={<SignUpForm navigate={ useNavigate()}/>} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<HomePage navigate={useNavigate()} />} />
+      <Route path="/signup" element={<SignUpForm navigate={useNavigate()} />} />
+      <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
+    </Routes>
   );
 };
 
---
+++ frontend/src/components/login/loginForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+const LoginForm = ({ navigate }) => {
+  const handleChange = (setFunction) => {
+    return (event) => {
+      setFunction(event.target.value);
+    };
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    let response = await fetch("/tokens", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    });
+
+    if (response.status !== 201) {
+      console.log("failed login");
+      navigate("/login");
+    } else {
+      console.log("successful login");
+      let data = await response.json();
+      window.localStorage.setItem("token", data.token);
+      navigate("/");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div id="login-email" className="mb-5 text-xl mx-auto">
+          <label for="login-email"></label>
+          <br />
+          <input
+            id="login-email-input"
+            className="pl-1 border-2 rounded peer"
+            data-cy="login-email"
+            type="text"
+            value={email}
+            onChange={handleChange(setEmail)}
+          />
+        </div>
+        <div id="login-password" className="mb-5 text-xl mx-auto">
+          <label for="login-password"></label>
+          <br />
+          <input
+            id="login-password-input"
+            className="pl-1 border-2 rounded peer"
+            data-cy="login-password"
+            type="password"
+            value={password}
+            onChange={handleChange(setPassword)}
+          />
+        </div>
+        <div>
+          <input
+            id="login-submit"
+            className="btn bg-green-600 border-0 hover:bg-green-700 rounded-full"
+            data-cy="login-submit"
+            type="submit"
+            value="submit"
+          />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
