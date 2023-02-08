5ed4c4e757b512f6d51ed569f37b795d36dc63a4
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-08 11:34:21 +0000

    Passing E2E that trips page populates with saved trip

--- frontend/cypress/e2e/user_trips.cy.js
+++ frontend/cypress/e2e/user_trips.cy.js
@@ -98,4 +98,52 @@ describe("Trips button", () => {
       cy.get('[data-cy="saveButton"]').should("exist");
     });
   });
+
+  it("when user logged in the save button is visible", () => {
+    cy.session(["newuser200@email.com", "12345678"], () => {
+      cy.request({
+        method: "POST",
+        url: "/tokens",
+        body: { email: "newuser200@email.com", password: "12345678" },
+      }).then(({ body }) => {
+        window.localStorage.setItem("token", body.token);
+      });
+    });
+
+    cy.intercept(
+      "GET",
+      "/emissions?from=London&to=Berlin&passengers=2",
+      (req) => {
+        req.reply({
+          statusCode: 200,
+          body: {
+            message: "OK",
+            emissions: {
+              plane: { total: 31.547396, perPassenger: 15.773698 },
+              petrolCar: { total: 30.547396, perPassenger: 14.773698 },
+              electricCar: { total: 29.547396, perPassenger: 13.773698 },
+              train: { total: 28.547396, perPassenger: 12.773698 },
+            },
+            from: "London, ENG, United Kingdom",
+            to: "Berlin, Germany",
+          },
+        });
+      }
+    ).as("getEmissions");
+
+    cy.visit("/");
+    cy.get('[data-cy="from"]').type("London");
+    cy.get('[data-cy="to"]').type("Berlin");
+    cy.get('[data-cy="passengers"]').type("2");
+    cy.get('[data-cy="travelFormSubmit"]').click();
+
+    cy.wait("@getEmissions").then(() => {
+      cy.get('[data-cy="saveButton"]').click();
+      cy.visit("/trips");
+      cy.get('[data-cy="trips"]').should(
+        "contain.text",
+        "From:London, ENG, United KingdomTo:Berlin, GermanyPassengers:2"
+      );
+    });
+  });
 });
